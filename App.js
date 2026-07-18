4ca3ed23aa15f66c8b275b7378970000c1b6549a
armanian <armanian@local>
2026-07-18 15:37:35 +0330

    Test with ThemeProvider

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
-import AppMinimal from './App.minimal';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { ThemeProvider } from './src/ThemeContext';
 
-export default AppMinimal;
+export default function App() {
+  return (
+    <ThemeProvider>
+      <View style={styles.container}>
+        <Text style={styles.text}>آرمک تسک - بعد ThemeProvider</Text>
+      </View>
+    </ThemeProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#A0C4FF' },
+  text: { fontSize: 20, fontWeight: '800', color: '#fff' },
+});
