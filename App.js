7236b603833260a5396d5ab9344bdcd781d275f5
armanian <armanian@local>
2026-07-19 08:53:59 +0330

    Test import calendar (jalali-moment)

--- App.js
+++ App.js
@@ -1,47 +1,21 @@
-import React, { useEffect } from 'react';
-import * as Notifications from 'expo-notifications';
-import HomeScreen from './src/screens/HomeScreen';
-import TasksScreen from './src/screens/TasksScreen';
-import CalendarScreen from './src/screens/CalendarScreen';
-import CategoriesScreen from './src/screens/CategoriesScreen';
-import ReportScreen from './src/screens/ReportScreen';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { ThemeProvider } from './src/ThemeContext';
-import { requestPermission, scheduleAllAlarms, scheduleSnooze } from './src/notifications';
-import CustomTabs from './src/CustomTabs';
-import ErrorBoundary from './src/ErrorBoundary';
+import * as Calendar from './src/calendar';
 
 export default function App() {
-  useEffect(() => {
-    init();
-    const sub = Notifications.addNotificationResponseReceivedListener((response) => {
-      const data = response.notification.request.content.data;
-      if (data && data.taskId && data.snooze) {
-        scheduleSnooze(data.taskId, data.snooze);
-      }
-    });
-    return () => sub.remove();
-  }, []);
-
-  async function init() {
-    try {
-      await requestPermission();
-      await scheduleAllAlarms();
-    } catch (e) {
-      console.log('init error:', e);
-    }
-  }
-
   return (
-    <ErrorBoundary>
-      <ThemeProvider>
-        <CustomTabs>
-          <CustomTabs.Screen name="home" component={HomeScreen} />
-          <CustomTabs.Screen name="tasks" component={TasksScreen} />
-          <CustomTabs.Screen name="calendar" component={CalendarScreen} />
-          <CustomTabs.Screen name="categories" component={CategoriesScreen} />
-          <CustomTabs.Screen name="report" component={ReportScreen} />
-        </CustomTabs>
-      </ThemeProvider>
-    </ErrorBoundary>
+    <ThemeProvider>
+      <View style={styles.container}>
+        <Text style={styles.text}>تست import calendar (jalali-moment)</Text>
+        <Text style={styles.sub}>{typeof Calendar.toJalali}</Text>
+      </View>
+    </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#A0C4FF', padding: 20 },
+  text: { fontSize: 18, fontWeight: '800', color: '#fff', textAlign: 'center' },
+  sub: { fontSize: 14, color: '#fff', marginTop: 10 },
+});
